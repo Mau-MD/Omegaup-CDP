d9a6093e76e81c7c48cb7f8d6d1d0afdc9843090
Mauricio Muñoz <a00832956@itesm.mx>
2021-06-24 18:14:32 -0700

    added edit case button and modal

--- src/Components/Cases/Input/Navbar.tsx
+++ src/Components/Cases/Input/Navbar.tsx
@@ -8,9 +8,13 @@ import {
   Flex,
   Spacer,
   Button,
+  useDisclosure,
 } from "@chakra-ui/react";
+import AddCase from "../Sidebar/AddCase";
+import EditCase from "./EditCase";
 
 const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const selectedCase = useStoreState((state) => state.selected.selected);
 
   return (
@@ -20,7 +24,18 @@ const Navbar = () => {
       </Text>
       <h2> {selectedCase?.groupName}</h2>
       <Spacer />
-      <Button size={"sm"}> Editar Caso </Button>
+      <Button size={"sm"} onClick={onOpen}>
+        {" "}
+        Editar Caso{" "}
+      </Button>
+      <EditCase
+        isOpen={isOpen}
+        onClose={onClose}
+        groupName={selectedCase ? selectedCase?.groupName : ""}
+        caseName={selectedCase ? selectedCase?.caseName : ""}
+        casePoints={1}
+        pointsDefined={false}
+      />
       <Button size={"sm"}> Eliminar Caso </Button>
     </HStack>
   );
